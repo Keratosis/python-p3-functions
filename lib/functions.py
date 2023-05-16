8e2f9ea82dbbbfe83a89bdb3024ae200392f4792
braxtonshaffie <braxtonshaffie@gmail.com>
2023-05-16 09:57:23 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -14,11 +14,9 @@ def greet_with_default(name="programmer"):
     print(f"Hello, {name}!")
 
 def add(num1, num2): 
-    return 100  
+    return num1+num2
+    
   
-
-def halve(number):
-    return 50
 
 def halve(number):
     return number/2
